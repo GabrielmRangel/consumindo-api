8bf2d466233ff668250e83631da196c4580e5081
Gabriel Mendes Rangel <gabrielmendesrangel2@gmail.com>
2023-04-26 21:37:05 -0300

    Aula 04

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/');
+async function buscaEndereco(cep){
+    try{
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCepConvertida = await consultaCep.json();
 
-console.log(consultaCep)
+        if(consultaCepConvertida.erro){
+            throw Error("CEP não encontrado!");
+        }
+        
+        console.log(consultaCepConvertida);
+
+        return consultaCepConvertida;
+    } catch (erro) {
+        console.log("CEP inválido!");
+    }
+}
+
+// let ceps = ['01001000', '02556110'];
+// let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+// Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
